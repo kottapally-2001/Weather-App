de71649f6a13d1d67a089840a10f064258830515
Saicharan Kottapally <saicharan.kottapally@gmail.com>
2025-09-03 16:20:45 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-const API_KEY = "de9006a5c10b6461bf3f797af3e03d67"; // e.g. Visual Crossing or OpenWeather
+const API_KEY = "de9006a5c10b6461bf3f797af3e03d67"; 
 
 export async function fetchWeather(location) {
   try {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
     const response = await axios.get(url);
-
-    // Extract only what we need
     const data = response.data;
     return {
       name: data.name,
